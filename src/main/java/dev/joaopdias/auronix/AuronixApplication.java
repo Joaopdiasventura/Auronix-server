a05d7cd4b090f4f7f01ccc302187dd07949a0a4d
João Paulo Dias <joaopdias.dev@gmail.com>
2026-05-16 19:19:18 -0300

    init commit

---
+++ src/main/java/dev/joaopdias/auronix/AuronixApplication.java
@@ -0,0 +1,13 @@
+package dev.joaopdias.auronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuronixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuronixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaopdias/auronix/AuronixApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaopdias.auronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuronixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
